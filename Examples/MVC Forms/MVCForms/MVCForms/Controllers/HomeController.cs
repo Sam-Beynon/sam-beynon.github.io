7f3a4293f26b40ae7c59fba2370cc5fff3dd6870
Sam Beynon <sam.j.beynon@outlook.com>
2020-03-13 08:45:51 +0000

    Added MVC Form example.

---
+++ Examples/MVC Forms/MVCForms/MVCForms/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using MVCForms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(FormModel model)
+        {
+            //model now contains all the data that was put into the form, so process it as needed.
+            //_repository.save(new entity??);
+
+            return View("Index");
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Examples/MVC Forms/MVCForms/MVCForms/Models/FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCForms.Models
+{
+    public class FormModel
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
